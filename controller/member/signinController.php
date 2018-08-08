f01eb4853b617dcf51d693ec579274a4fd9e2999
Einere <kjwsx23@gamil.com>
2018-08-08 16:38:31 +0900

    Implement agree & login

--- controller/member/signinController.php
+++ controller/member/signinController.php
@@ -1,5 +1,7 @@
 <?php
     include '../../query/connect.php';
+    include '../../query/member/memberinsert.php';
+    include '../../query/member/memberselect.php';
     $dbConnect = new connect();
     $conn = $dbConnect->get_conn();
     // $this->load->library('form_validation');
@@ -20,8 +22,7 @@
     $phonenum = $_POST['phphonenum']; 
     $phonenum2 = $_POST['phphonenum2'];
     $agree = $_POST['memagree'];
- 
-    
+
     if(strlen($id) == 0){
         echo "<script>alert(\"아이디를 입력하세요\");location.replace('../../view/member/signinView.php');</script>";
      }
@@ -152,80 +153,34 @@
             }
     }
 
-
+    if($agree != "agree"){
+        echo "<script>alert(\"동의해주세요\");location.replace('../../view/member/signinView.php');</script>";
+    }
+    else{
+        $agree = 1;
+    }
     
-
-
-
-
-    echo "<script>alert(\"회원가입 되었습니다!\");location.replace('../../view/board/listView.php');</script>";
-
-
-    /////////////////////////////////여기부터 해야됨
-
-        
-    // $this->form_validation->set_rules('eemail', 'Email', 'required|trim|is_unique[emaillist.eemail]');
-    // $this->form_validation->set_rules('eemail2', 'Email2', 'trim|is_unique[emaillist.eemail]');
-        
-    // $this->form_validation->set_rules('phphonenum', 'Phone number', 'required|trim|numeric|min_length[11]|is_unique[phone.phphonenum]');
-    // $this->form_validation->set_rules('phphonenum', 'Phone number', 'trim|numeric|min_length[11]|is_unique[phone.phphonenum]');    
-
-    // $this->form_validation->set_rules('memnickname', 'Nick name', 'required|trim|is_unique[member.memnickname]'); 
-
-    // $this->form_validation->set_rules('memagree', 'Agree', 'required|trim|xss_clean');
-
-    // //회원가입 성공
-    // if ($this->form_validation->run())  
-    // {   
-    //     //echo "Welcome, you are logged in.";
-
-    //     //넘어온 데이터 저장
-    //     $id = $this->input->post('memid'); 
-    //     $pw = $this->input->post('mempw');
-    //     $firstname = $this->input->post('memfirstname');
-    //     $lastname = $this->input->post('memlastname');
-    //     $birth = $this->input->post('membirth');
-    //     $addr = $this->input->post('memaddr');
-    //     $nickname = $this->input->post('memnickname');
-    //     $email = $this->input->post('eemail'); 
-    //     $email2 = $this->input->post('eemail2'); 
-    //     $phonenum = $this->input->post('phphonenum'); 
-    //     $phonenum2 = $this->input->post('phphonenum2');
-    //     $agree = $this->input->post('memagree');
-    //     if($agree=="Agree") {
-    //         $agree = 1;
-    //     }
-    //     else{
-    //         $agree = 0;
-    //     }
-
-    //    //member table에 저장
-    //     $this->load->library('query/modules/member/memberinsert');
-    //     $this->memberinsert->mem_insert($this->conn, $id, $pw, $firstname, $lastname, $birth, $addr, $nickname, $agree);
-
-    //     $this->load->library('query/modules/member/memberselect');
-    //     $row = $this->memberselect->select_memseq($this->conn, $id);
-    //     $memseq = $row['memseq'];
-
-    //     $this->memberinsert->email_insert($this->conn, $memseq, $email);
-
-    //     //email이 두개면
-    //     if($email2!=NULL){
-    //         $this->memberinsert->email_insert($this->conn, $memseq, $email2);
-    //     }
-
-    //     $this->memberinsert->phone_insert($this->conn, $memseq, $phonenum);
-    //     //phonenum이 두개면
-    //      if($phonenum2!=NULL){
-    //         $this->memberinsert->phone_insert($this->conn, $memseq, $phonenum2);
-    //      }
-    //      echo "<script>alert('회원가입 성공!');</script>";
-    //      $this->load->view('../../view/board/listView');
-    // } 
-
-    // //회원가입 실패
-    // else {  
-    //     $this->load->view('../../view/member/signinView');  
-    // }  
-
+    //echo "$id, $pw, $firstname, $lastname, $birth, $addr, $nickname, $agree";
+    //data insert
+    $memInsert = new memberinsert();
+    $memInsert->mem_insert($conn, $id, $pw, $firstname, $lastname, $birth, $addr, $nickname, $agree);
+    echo "a";
+    $memSelect = new memberselect();
+    $row = $memSelect->select_memseq($conn, $id);
+    $memseq = $row['memseq'];
+    echo "b";
+    //email insert
+    $memInsert->email_insert($conn, $memseq, $email);
+    if($email2!=NULL){
+        $memInsert->email_insert($conn, $memseq, $email2);
+    }
+    echo "c";
+    //phone insert
+    $memInsert->phone_insert($conn, $memseq, $phonenum);
+    echo "d";
+    if($phonenum2!=NULL){
+        $memInsert->phone_insert($conn, $memseq, $phonenum2);
+    }
+    echo "e";
+    echo "<script>alert(\"회원가입 되었습니다!\"); location.replace('../../controller/board/boardController.php');</script>";
 ?>
---
+++ modules/member/memberList.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <meta></meta>
+    <title></title>
+</head>
+<body>
+    <ul>
+        <?php
+        ?>
+    </ul>
+</body>
+</html>
+

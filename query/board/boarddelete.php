9ffcfa3a31318293aa049153aa13dd5fe4d6e627
Einere <kjwsx23@gmail.com>
2018-08-10 14:18:42 +0900

    modify back link url at signin view

--- query/board/boarddelete.php
+++ query/board/boarddelete.php
@@ -4,7 +4,7 @@ class boarddelete {
     function post_delete($conn, $postseq) {
         mysqli_query($conn, "
         DELETE FROM post WHERE postseq = $postseq
-         ");
+        ");
     } 
     
 }  
